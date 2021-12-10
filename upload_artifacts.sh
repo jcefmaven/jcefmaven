501972944ba60d3ae0208cc5ebcd97ced7e99471
FriwiDev <friwidev@gmail.com>
2021-12-10 19:27:46 +0100

    Add url and repositoryId

--- upload_artifacts.sh
+++ upload_artifacts.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ ! $# -eq 1 ]
+if [ ! $# -eq 3 ]
   then
-    echo "Usage: ./upload_artifacts.sh <build_meta_url>"
+    echo "Usage: ./upload_artifacts.sh <build_meta_url> <repo_url> <repo_id>"
     echo ""
     echo "build_meta_url: The url to download build_meta.json from"
+    echo "repo_url: The repository to push to"
+    echo "repo_id: A short identifier for the repo to push to"
     exit 1
 fi
 
@@ -19,8 +21,8 @@ export $(curl -s -L $1 | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.
 
 echo "Uploading maven artifacts for $release_tag..."
 
-mvn gpg:sign-and-deploy-file -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag.jar
-mvn gpg:sign-and-deploy-file -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag-sources.jar -Dclassifier=sources
-mvn gpg:sign-and-deploy-file -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag-javadoc.jar -Dclassifier=javadoc
+mvn gpg:sign-and-deploy-file -Durl=$2 -DrepositoryId=$3 -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag.jar
+mvn gpg:sign-and-deploy-file -Durl=$2 -DrepositoryId=$3 -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=$2 -DrepositoryId=$3 -DpomFile=jcef-api-$release_tag.pom -Dfile=jcef-api-$release_tag-javadoc.jar -Dclassifier=javadoc
 
 echo "Done uploading maven artifacts!"
