11c11850f7b5682d7aa474d250252188214e273d
FriwiDev <friwidev@gmail.com>
2021-12-17 08:02:41 +0100

    Fix auto closing of staging repository

--- close_repository.sh
+++ close_repository.sh
@@ -15,21 +15,18 @@ cd "$( dirname "$0" )"
 #List repos
 echo "Fetching repositories..."
 repolist=$(curl -s -u "$1:$2" 'https://oss.sonatype.org/service/local/staging/profile_repositories?_dc=1639716203993' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0' -H 'Accept: application/json,application/vnd.siesta-error-v1+json,application/vnd.siesta-validation-errors-v1+json' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'X-Nexus-UI: true' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Referer: https://oss.sonatype.org/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: trailers')
-repos=$(echo '$repolist' | grep -oP '(?<=repositoryId\":\").*?(?=\",\"type\":\"open)')
 echo "$repolist"
 echo ""
-echo "Repositories: "
-echo "$repos"
-echo ""
+
+repolist=$(sed 's|\"|\n|g' <<< $repolist)
 
 #Close all open repos
-echo "Closing all open repos..."
+echo "Closing all repos..."
 while IFS= read -r repo; do
-    if [[ "$repo" == '' ]]; then
-      break
+    if [[ $repo == mefriwi* ]] ; then
+      echo "Closing $repo"
+      curl -s -u "$1:$2" 'https://oss.sonatype.org/service/local/staging/bulk/close' -X POST -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0' -H 'Accept: application/json,application/vnd.siesta-error-v1+json,application/vnd.siesta-validation-errors-v1+json' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'X-Nexus-UI: true' -H 'Content-Type: application/json' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://oss.sonatype.org' -H 'Connection: keep-alive' -H 'Referer: https://oss.sonatype.org/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: trailers' --data-raw "{\"data\":{\"description\":\"Closing $repo\",\"stagedRepositoryIds\":[\"$repo\"]}}"
     fi
-    echo "Closing $repo"
-    curl -s -u "$1:$2" 'https://oss.sonatype.org/service/local/staging/bulk/close' -X POST -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0' -H 'Accept: application/json,application/vnd.siesta-error-v1+json,application/vnd.siesta-validation-errors-v1+json' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'X-Nexus-UI: true' -H 'Content-Type: application/json' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://oss.sonatype.org' -H 'Connection: keep-alive' -H 'Referer: https://oss.sonatype.org/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: trailers' --data-raw $'{"data":{"description":"Closing $repo","stagedRepositoryIds":["$repo"]}}'
-done <<< "$repos"
+done <<< "$repolist"
 
-echo "All open repos closed!"
+echo "All repos closed!"
