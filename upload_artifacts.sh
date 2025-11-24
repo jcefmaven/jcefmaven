545b23cf04a048f507868ce36880b9ddf46b0329
Fritz Windisch <friwidev@gmail.com>
2025-11-24 17:31:41 +0100

    Test

--- upload_artifacts.sh
+++ upload_artifacts.sh
@@ -19,11 +19,7 @@ rm -rf upload
 mkdir upload
 cp out/* upload/
 
-echo "Uploading maven artifacts for $mvn_version..."
-
-#Sign Jogamp libraries
-./sign_artifact.sh me.friwi jogl-all $jogl_build
-./sign_artifact.sh me.friwi gluegen-rt $jogl_build
+echo "Deploying maven artifacts for $mvn_version..."
 
 #Check for duplicates among jogamp libraries
 metadata=$(curl -s "https://repo1.maven.org/maven2/me/friwi/jogl-all/maven-metadata.xml")
@@ -35,27 +31,17 @@ if echo "$metadata" | grep -q "<version>$jogl_build</version>"; then
   rm gluegen-rt*
 fi
 
-#Sign API
-./sign_artifact.sh me.friwi jcef-api $release_tag
+# Sign all artifacts
+echo "Signing artifacts..."
+for file in ./upload/*
+do
+  echo "Signing $file..."
+  gpg --detach-sign --armor --no-tty "$file"
+done
 
-#Sign jcefmaven
-./sign_artifact.sh me.friwi jcefmaven $mvn_version
-
-#Sign linux natives
-./sign_artifact.sh me.friwi jcef-natives-linux-amd64 $release_tag
-./sign_artifact.sh me.friwi jcef-natives-linux-arm64 $release_tag
-./sign_artifact.sh me.friwi jcef-natives-linux-arm $release_tag
-
-#Sign windows natives
-./sign_artifact.sh me.friwi jcef-natives-windows-amd64 $release_tag
-./sign_artifact.sh me.friwi jcef-natives-windows-arm64 $release_tag
-./sign_artifact.sh me.friwi jcef-natives-windows-i386 $release_tag
-
-#Sign macosx natives
-./sign_artifact.sh me.friwi jcef-natives-macosx-amd64 $release_tag
-./sign_artifact.sh me.friwi jcef-natives-macosx-arm64 $release_tag
 
 #Upload all
+echo "Uploading artifacts..."
 mvn central-publishing-maven-plugin:deploy -DstagingDirectory=./upload
 
 echo "Done uploading maven artifacts!"
